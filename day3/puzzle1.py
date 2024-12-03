eefe3c2c762ce9da93450512c6df6b1f59c4a8a6
Peter Hagelund <peterhagelund@mac.com>
2024-12-03 10:15:55 -0500

    minor change to use regex capture groups

--- day3/puzzle1.py
+++ day3/puzzle1.py
@@ -3,13 +3,12 @@ import re
 
 def main():
     with open('puzzle_input.txt', 'rt') as f:
-        sum = 0
+        total = 0
         for line in f:
-            instructions = re.findall(r'mul\([0-9]{1,3},[0-9]{1,3}\)', line)
-            for instruction in instructions:
-                values = [int(v) for v in instruction[4:-1].split(',')]
-                sum += values[0] * values[1]
-    print(f'sum = {sum}')
+            instructions = re.findall(r'mul\(([0-9]{1,3}),([0-9]{1,3})\)', line)
+            values = [(int(i[0]), int(i[1])) for i in instructions]
+            total += sum([v[0] * v[1] for v in values])
+    print(f'sum = {total}')
 
 
 if __name__ == '__main__':
