9ce3db997be53053a7ee4a5d9a3c4da04df7fc18
Peter Hagelund <peterhagelund@mac.com>
2024-12-05 12:11:03 -0500

    cleanup

--- day2/part1.py
+++ day2/part1.py
@@ -1,13 +1,3 @@
-def main():
-    safe = 0
-    with open('puzzle_input.txt', 'rt') as f:
-        for line in f:
-            levels = [int(l) for l in line.strip().split()]
-            if is_safe(levels):
-                safe += 1
-    print(f'safe = {safe}')
-
-
 def is_safe(levels: list[int]) -> bool:
     asc = sorted(levels)
     desc = sorted(levels, reverse=True)
@@ -18,6 +8,16 @@ def is_safe(levels: list[int]) -> bool:
         if delta < 1 or delta > 3:
             return False
     return True
+
+
+def main():
+    answer = 0
+    with open('puzzle_input.txt', 'rt') as f:
+        for line in f:
+            levels = [int(l) for l in line.strip().split()]
+            if is_safe(levels):
+                answer += 1
+    print(f'answer = {answer}')
 
 
 if __name__ == '__main__':
