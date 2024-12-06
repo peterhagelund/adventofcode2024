e46de1254937485f670fa3fbe54cc8ca586b3c0c
Peter Hagelund <peterhagelund@mac.com>
2024-12-06 14:33:28 -0500

    optimizations

--- day6/part1.py
+++ day6/part1.py
@@ -1,55 +1,37 @@
-from enum import Enum
+GUARD = ord('^')
+OBSTRUCTION = ord('#')
+DELTAS = [
+    (-1, 0),  # up
+    (1, 0),  # down
+    (0, -1),  # left
+    (0, 1),  # right
+]
 
-
-class Direction(Enum):
-    UP = 0
-    DOWN = 1
-    RIGHT = 2
-    LEFT = 3
-
-
-def find_guard_start(map: list[str]) -> tuple[int, int] | None:
-    for y in range(len(map)):
-        for x in range(len(map[y])):
-            if map[y][x] == '^':
-                return (y, x)
-    return None
+DIRECTIONS = [3, 2, 0, 1]
 
 
 def main():
-    map: list[str] = []
+    map: list[bytearray] = []
+    pos: tuple[int, int] | None = None
+    y = 0
     with open('puzzle_input.txt', 'rt') as f:
         for line in f:
-            map.append(line.strip())
-    pos = find_guard_start(map)
-    assert pos is not None
+            row = bytearray(line.strip(), 'utf-8')
+            x = row.find(GUARD)
+            if x != -1:
+                pos = (y, x)
+            map.append(row)
+            y += 1
     (y, x) = pos
-    height = len(map)
-    width = len(map[0])
+    height, width = len(map), len(map[0])
     visited: set[tuple[int, int]] = {(y, x)}
-    direction = Direction.UP
+    direction = 0
     while True:
-        match direction:
-            case Direction.UP:
-                _y, _x = y - 1, x
-            case Direction.DOWN:
-                _y, _x = y + 1, x
-            case Direction.LEFT:
-                _y, _x = y, x - 1
-            case Direction.RIGHT:
-                _y, _x = y, x + 1
+        _y, _x = y + DELTAS[direction][0], x + DELTAS[direction][1]
         if _y < 0 or _y == height or _x < 0 or _x == width:
             break
-        if map[_y][_x] == '#':
-            match direction:
-                case Direction.UP:
-                    direction = Direction.RIGHT
-                case Direction.DOWN:
-                    direction = Direction.LEFT
-                case Direction.LEFT:
-                    direction = Direction.UP
-                case Direction.RIGHT:
-                    direction = Direction.DOWN
+        if map[_y][_x] == OBSTRUCTION:
+            direction = DIRECTIONS[direction]
         else:
             y, x = _y, _x
             visited.add((y, x))
--- day6/part2.py
+++ day6/part2.py
@@ -1,82 +1,70 @@
-from enum import Enum
+GUARD = ord('^')
+DOT = ord('.')
+OBSTRUCTION = ord('#')
+DELTAS = [
+    (-1, 0),  # up
+    (1, 0),  # down
+    (0, -1),  # left
+    (0, 1),  # right
+]
+
+DIRECTIONS = [3, 2, 0, 1]
 
 
-class Direction(Enum):
-    UP = 0
-    DOWN = 1
-    RIGHT = 2
-    LEFT = 3
-
-
-def find_guard_start(map: list[str]) -> tuple[int, int] | None:
-    for y in range(len(map)):
-        for x in range(len(map[y])):
-            if map[y][x] == '^':
-                return (y, x)
-    return None
-
-
-def walk_guard_route(map: list[str], y: int, x: int) -> bool:
-    turns: set[tuple[int, int, Direction]] = set()
+def walk_guard_route(map: list[bytearray], y: int, x: int, visits=False) -> tuple[set[tuple[int, int]], bool]:
+    visited: set[tuple[int, int]] | None = None
+    if visits:
+        visited = {(x, y)}
     loop = False
+    turns: set[tuple[int, int, int]] | None = None
+    if not visits:
+        turns = set()
     height = len(map)
     width = len(map[0])
-    direction = Direction.UP
+    direction = 0
     while True:
-        match direction:
-            case Direction.UP:
-                _y, _x = y - 1, x
-            case Direction.DOWN:
-                _y, _x = y + 1, x
-            case Direction.LEFT:
-                _y, _x = y, x - 1
-            case Direction.RIGHT:
-                _y, _x = y, x + 1
+        _y, _x = y + DELTAS[direction][0], x + DELTAS[direction][1]
         if _y < 0 or _y == height or _x < 0 or _x == width:
             break
-        if map[_y][_x] == '#':
-            turn = (y, x, direction)
-            if turn in turns:
-                loop = True
-                break
-            turns.add(turn)
-            match direction:
-                case Direction.UP:
-                    direction = Direction.RIGHT
-                case Direction.DOWN:
-                    direction = Direction.LEFT
-                case Direction.LEFT:
-                    direction = Direction.UP
-                case Direction.RIGHT:
-                    direction = Direction.DOWN
+        if map[_y][_x] == OBSTRUCTION:
+            if not visits:
+                turn = (y, x, direction)
+                if turn in turns:
+                    loop = True
+                    break
+                turns.add(turn)
+            direction = DIRECTIONS[direction]
         else:
             y, x = _y, _x
-    return loop
+            if visits:
+                visited.add((y, x))
+    return (visited, loop)
 
 
 def main():
-    map: list[str] = []
+    map: list[bytearray] = []
+    pos: tuple[int, int] | None = None
+    y = 0
     with open('puzzle_input.txt', 'rt') as f:
         for line in f:
-            map.append(line.strip())
-    pos = find_guard_start(map)
+            row = bytearray(line.strip(), 'utf-8')
+            x = row.find(GUARD)
+            if x != -1:
+                pos = (y, x)
+            map.append(row)
+            y += 1
     assert pos is not None
     (start_y, start_x) = pos
+    visited, _ = walk_guard_route(map, start_y, start_x, visits=True)
     answer = 0
-    for y in range(len(map)):
-        for x in range(len(map[y])):
-            if map[y][x] != '.':
-                continue
-            _map: list[str] = []
-            for i in range(len(map)):
-                row = map[i]
-                if i != y:
-                    _map.append(row)
-                else:
-                    _map.append(row[:x] + '#' + row[x + 1 :])
-            loop = walk_guard_route(_map, start_y, start_x)
-            if loop:
-                answer += 1
+    for y, x in visited:
+        if y == start_y and x == start_x:
+            continue
+        map[y][x] = OBSTRUCTION
+        _, loop = walk_guard_route(map, start_y, start_x)
+        map[y][x] = DOT
+        if loop:
+            answer += 1
     print(f'answer = {answer}')
 
 
